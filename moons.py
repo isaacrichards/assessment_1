e066c0039382c6be085fc952b67147c396502f97
Isaac Richards <yh22108@bristol.ac.uk>
2024-01-06 18:52:56 +0000

    Initial pass of moons class, including a names and data method

--- moons.py
+++ moons.py
@@ -5,9 +5,8 @@ class Moons:
     def __init__(self):
 
         self.data = self.data()#attribute the whole data base to Moons.data
-        #self.names = self.names()
-
-
+        self.names = self.names()
+        
     def data(self):
 
         database_service = "sqlite"
@@ -16,11 +15,24 @@ class Moons:
 
         connectable = f"{database_service}:///{database}"
 
-        query = "SELECT * FROM *"
+        query = "SELECT * FROM moons"
 
-        selected_data = pd.read_sql(query, connectable)            
+        selected_data = pd.read_sql(query, connectable)   
+        
+        selected_data.set_index("moon", inplace = True)
 
         return selected_data
-
-    #def names(self):      
     
+    
+    def names(self):
+        
+        name_list = []
+        
+        for name in self.data.index:
+            
+            name_list.append(name)
+        
+        return name_list
+
+    #def names(self):
+
